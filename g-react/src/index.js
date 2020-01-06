2838f8cb72e350b4e59a872be3eb34d4b4e543a2
baljnyam <baljnyam@gmail.com>
2020-01-06 14:45:03 +1100

    react-snap

--- g-react/src/index.js
+++ g-react/src/index.js
@@ -5,5 +5,12 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
 
 serviceWorker.register();
