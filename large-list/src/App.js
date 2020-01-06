ac4cbbe1575f119d022820fe48a6c92da51739ac
baljnyam <baljnyam@gmail.com>
2020-01-06 13:27:04 +1100

    large-list-app-row

--- large-list/src/App.js
+++ large-list/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ListComponent from "./ListComponent";
+
+import initialItems from "./mock.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: initialItems,
+      moreItemsLoading: false,
+      hasNextPage: true
+    };
+
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  loadMore() {
+    this.setState({ moreItemsLoading: true }, () => {
+      fetch("https://dog.ceo/api/breeds/image/random/10")
+        .then(res => res.json())
+        .then(({ message: newItems }) =>
+          this.setState({
+            moreItemsLoading: false,
+            items: [...this.state.items, ...newItems]
+          })
+        );
+    });
+  }
+
+  render() {
+    const { items, moreItemsLoading, hasNextPage } = this.state;
+
+    return (
+      <>
+        <ListComponent
+          items={items}
+          moreItemsLoading={moreItemsLoading}
+          loadMore={this.loadMore}
+          hasNextPage={hasNextPage}
+        />
+        <div className="title">
+          <h1>InfiniteLoader | react-window</h1>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
