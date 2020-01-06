c7d9a2f9477be0d2dc0263e5193f07e25abe41f3
baljnyam <baljnyam@gmail.com>
2020-01-06 14:53:38 +1100

    snap

--- g-react/src/index.js
+++ g-react/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 const rootElement = document.getElementById("root");
 
 if (rootElement.hasChildNodes()) {
@@ -12,5 +11,4 @@ if (rootElement.hasChildNodes()) {
 } else {
   ReactDOM.render(<App />, rootElement);
 }
-
 serviceWorker.register();
