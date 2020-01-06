af3248ebace0cc60f3688e13b9b8964a837e4fb2
baljnyam <baljnyam@gmail.com>
2020-01-06 11:15:27 +1100

    change

--- g-react/src/App.js
+++ g-react/src/App.js
@@ -4,6 +4,37 @@ const AvatarComponent = lazy(() => import("./AvatarComponent"));
 const InfoComponent = lazy(() => import("./InfoComponent"));
 const MoreInfoComponent = lazy(() => import("./MoreInfoComponent"));
 
+const renderLoader = () => <p>Loading</p>;
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Loading failed! Please reload.</p>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const DetailsComponent = () => (
+  <ErrorBoundary>
+    <Suspense fallback={renderLoader()}>
+      <AvatarComponent />
+      <InfoComponent />
+      <MoreInfoComponent />
+    </Suspense>
+  </ErrorBoundary>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
